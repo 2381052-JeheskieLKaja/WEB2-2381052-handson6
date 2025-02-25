fb9f7da4c5872da2a5b23afa2bed5ba097351b92
Jeheskiel Kaja <2381052@unai.edu>
2025-02-25 19:08:51 +0700

    perbarui handson 6

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import axios from "../utils/AxiosInstance";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "../utils/AxiosInstance";
 import { useEffect } from "react";
 
 interface ProductDetail {
@@ -50,7 +50,7 @@ interface DeletedProduct extends ProductDetail {
   deletedOn: string;
 }
 
-const fetchProductDetail = async (id: string | undefined) => {
+export const fetchProductDetail = async (id: string | undefined) => {
   return await axios.get<ProductDetail>(`/product/${id}`);
 };
 
@@ -141,29 +141,56 @@ const ProductDetailSkeleton = () => {
     </div>
   );
 };
-
 const ProductDetail = () => {
   const { id } = useParams();
   const getProductDetail = useQuery({
     queryKey: ["productDetail", id],
-    queryFn: () => fetchProductDetail(id),
+    queryFn: () => fetchProductDetail(id)
+  });
+  const deleteProductMutation = useMutation({
+    mutationFn: () => deleteProduct(id)
   });
   const product: ProductDetail | undefined = getProductDetail.data?.data;
-  const deleteProductMutation = useMutation({
-    mutationFn: () => deleteProduct(id),
-  });
   const navigate = useNavigate();
+
   useEffect(() => {
     if (deleteProductMutation.isSuccess) {
       navigate("/product", { replace: true });
     }
   }, [deleteProductMutation.isSuccess]);
   return (
-    <>
-      {getProductDetail.isFetching || product == undefined ? (
+    <div>
+      {getProductDetail.isFetching || product === undefined ? (
         <ProductDetailSkeleton />
       ) : (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 relative">
+          {deleteProductMutation.isPending && (
+            <div className="absolute inset-0 bg-white/50 backdrop-blur-sm z-10 flex items-center justify-center">
+              <div className="flex items-center bg-white/90 px-6 py-3 rounded-lg shadow-lg">
+                <span className="text-2xl mr-4 text-gray-800">Deleting...</span>
+                <svg
+                  className="animate-spin h-5 w-5 text-gray-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          )}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {/* Product Image */}
             <div className="flex justify-center">
@@ -292,53 +319,51 @@ const ProductDetail = () => {
                   </p>
                 </div>
               ))}
-              <div className="fixed bottom-4 right-4 z-50">
-                <div className="relative group">
-                  <button className="bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                      />
-                    </svg>
-                  </button>
-                  <div className="absolute bottom-14 right-0 bg-white rounded-lg shadow-lg w-32 hidden group-focus-within:block">
-                    <button
-                      onClick={() => {
-                        // navigate("edit");
-                      }}
-                      className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                      onClick={() => {
-                        if (
-                          confirm("Are you sure want to delete this product ? ")
-                        ) {
-                            deleteProductMutation.mutate();
-                        }
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
       )}
-    </>
+      <div className="fixed bottom-4 right-4 z-50">
+        <div className="relative group">
+          <button className="bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+              />
+            </svg>
+          </button>
+          <div className="absolute bottom-14 right-0 bg-white rounded-lg shadow-lg w-32 hidden group-focus-within:block">
+            <button
+              onClick={() => {
+                navigate("edit");
+              }}
+              className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+            >
+              Edit
+            </button>
+            <button
+              className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+              onClick={() => {
+                if (confirm("Are you sure want to delete this product ? ")) {
+                  deleteProductMutation.mutate();
+                }
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
